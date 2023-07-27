7c54596d3d41357ff37bca073f68d4834153db63
madhav431 <madhavpampana311@gmail.com>
2023-07-27 05:49:23 +0000

    pushing sql queries

---
+++ sqlqueries_madhav.sql
@@ -0,0 +1,15 @@
+create index sales_idx on sales( PRICEEACH, YEAR_ID, COUNTRY);
+
+select count(distinct CUSTOMERNAME) as CustomerName from sales where COUNTRY = 'USA';
+
+select COUNT(distinct CUSTOMERNAME) as CountCustomer from sales where SALES > 5000;
+
+
+select count(PRODUCTCODE) as ProductCount from sales
+where YEAR_ID = 2003;
+
+
+select sum(SALES) as TotalSalePrice from sales 
+where YEAR_ID = 2005;
+
+select YEAR_ID, sum(sales) as totalsaleprice from sales group by YEAR_ID;
