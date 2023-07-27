6d4d74f7e65572c77a00bd655e34929e05fd675c
madhav431 <madhavpampana311@gmail.com>
2023-07-27 10:19:31 +0000

    modified queries

--- sqlqueries_madhav.sql
+++ sqlqueries_madhav.sql
@@ -1,15 +1,15 @@
 create index sales_idx on sales( PRICEEACH, QUANTITYORDERED, YEAR_ID, COUNTRY);
 
-select count(distinct CUSTOMERNAME) as CustomerName from sales where COUNTRY = 'USA';
+select COUNT(distinct CUSTOMERNAME) as CustomerName from sales where COUNTRY = 'USA';
 
 select COUNT(distinct CUSTOMERNAME) as CountCustomer from sales where PRICEEACH * QUANTITYORDERED > 5000;
 
 
-select count(distinct PRODUCTCODE) as ProductCount from sales
+select COUNT(distinct PRODUCTCODE) as ProductCount from sales
 where YEAR_ID = 2003;
 
 
-select sum(SALES) as TotalSalePrice from sales 
+select SUM(SALES) as TotalSalePrice from sales 
 where YEAR_ID = 2005;
 
-select YEAR_ID, sum(sales) as totalsaleprice from sales group by YEAR_ID;
+select YEAR_ID, SUM(sales) as totalsaleprice from sales group by YEAR_ID;
